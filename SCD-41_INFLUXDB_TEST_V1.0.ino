8765d2dda9b153d29ce7053d0cb85c9a6a47e08c
Shazaib <56131964+alpha815@users.noreply.github.com>
2022-05-27 18:39:13 +0500

    Add files via upload

---
+++ SCD-41_INFLUXDB_TEST_V1.0.ino
@@ -0,0 +1,195 @@
+/*
+ * FIXED INT TO FLOAT ISSUE WITH INFLUXDB AND FIELD IS NOW SCD41_C02
+   Copyright (c) 2021, Sensirion AG
+   All rights reserved.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are met:
+
+ * * Redistributions of source code must retain the above copyright notice, this
+     list of conditions and the following disclaimer.
+
+ * * Redistributions in binary form must reproduce the above copyright notice,
+     this list of conditions and the following disclaimer in the documentation
+     and/or other materials provided with the distribution.
+
+ * * Neither the name of Sensirion AG nor the names of its
+     contributors may be used to endorse or promote products derived from
+     this software without specific prior written permission.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+   ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+   LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+   INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+   CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+   ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+   POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include <Arduino.h>
+#include <SensirionI2CScd4x.h>
+#include <Wire.h>
+SensirionI2CScd4x scd4x;
+
+#include <InfluxDbClient.h>
+#include <InfluxDbCloud.h>
+
+#define WIFI_SSID "WiFi SSID "
+#define WIFI_PASSWORD "WiFi Password"
+/*
+Uncomment next lines for static IP
+*/
+/*
+IPAddress local_IP(192, 168, 1, 3);
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 255, 255, 0);
+IPAddress primaryDNS(8, 8, 8, 8);
+IPAddress secondaryDNS(8, 8, 4, 4);
+*/
+#define INFLUXDB_URL "http://141.95.1.50:8086"
+#define INFLUXDB_DB_NAME "mydb"
+Point Sensor("Air_Quality");
+InfluxDBClient client(INFLUXDB_URL, INFLUXDB_DB_NAME);
+#define Sensor_Name "SCD-41"
+boolean ledState = false;
+int ledPin = 2;
+int interv = 500;
+long previousMillis = 0;
+long currentMillis;
+
+void printUint16Hex(uint16_t value) {
+  Serial.print(value < 4096 ? "0" : "");
+  Serial.print(value < 256 ? "0" : "");
+  Serial.print(value < 16 ? "0" : "");
+  Serial.print(value, HEX);
+}
+
+void printSerialNumber(uint16_t serial0, uint16_t serial1, uint16_t serial2) {
+  Serial.print("Serial: 0x");
+  printUint16Hex(serial0);
+  printUint16Hex(serial1);
+  printUint16Hex(serial2);
+  Serial.println();
+}
+
+void setup() {
+
+  Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
+/*
+Uncomment next lines for static IP
+*/
+// WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS);
+  Serial.print("Connecting to ");
+  Serial.println(WIFI_SSID);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.println();
+  while (WiFi.status() != WL_CONNECTED) {
+    currentMillis = millis();
+    if (currentMillis - previousMillis > interv)
+    {
+      previousMillis = currentMillis;
+      ledState = !ledState;
+      digitalWrite(ledPin, ledState);
+    }
+  }
+  Serial.println("Connected to WiFi ");
+  Serial.println(WIFI_SSID);
+  client.setConnectionParamsV1(INFLUXDB_URL, INFLUXDB_DB_NAME);
+
+  if (client.validateConnection()) {
+    Serial.print("Connected to InfluxDB: ");
+    Serial.println(client.getServerUrl());
+  } else {
+    Serial.print("InfluxDB connection failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
+    Sensor.addTag("Sensor", Sensor_Name);
+
+  Wire.begin();
+
+
+  uint16_t error;
+  char errorMessage[256];
+
+  scd4x.begin(Wire);
+
+  // stop potentially previously started measurement
+  error = scd4x.stopPeriodicMeasurement();
+  if (error) {
+    Serial.print("Error trying to execute stopPeriodicMeasurement(): ");
+    errorToString(error, errorMessage, 256);
+    Serial.println(errorMessage);
+  }
+
+  uint16_t serial0;
+  uint16_t serial1;
+  uint16_t serial2;
+  error = scd4x.getSerialNumber(serial0, serial1, serial2);
+  if (error) {
+    Serial.print("Error trying to execute getSerialNumber(): ");
+    errorToString(error, errorMessage, 256);
+    Serial.println(errorMessage);
+  } else {
+    printSerialNumber(serial0, serial1, serial2);
+  }
+
+  // Start Measurement
+  error = scd4x.startPeriodicMeasurement();
+  if (error) {
+    Serial.print("Error trying to execute startPeriodicMeasurement(): ");
+    errorToString(error, errorMessage, 256);
+    Serial.println(errorMessage);
+  }
+
+  Serial.println("Waiting for first measurement... (5 sec)");
+}
+
+void loop() {
+  if (WiFi.status() != WL_CONNECTED) {                       // If no Wifi signal, try to reconnect it
+    Serial.println("Wifi connection lost");
+    Serial.println("Reconnecting to WiFi...");
+    WiFi.disconnect();
+    WiFi.reconnect();
+  }
+  uint16_t error;
+  char errorMessage[256];
+
+  delay(5000);
+
+  // Read Measurement
+  uint16_t co2;
+  float temperature;
+  float humidity;
+  error = scd4x.readMeasurement(co2, temperature, humidity);
+  if (error) {
+    Serial.print("Error trying to execute readMeasurement(): ");
+    errorToString(error, errorMessage, 256);
+    Serial.println(errorMessage);
+  } else if (co2 == 0) {
+    Serial.println("Invalid sample detected, skipping.");
+  } else {
+    Serial.print("Co2:");
+    Serial.print(co2);
+    float scd41_co2 = co2;
+    Sensor.addField("SCD41_CO2", scd41_co2);
+    Serial.print("\t");
+    Serial.print("Temperature:");
+    Serial.print(temperature);
+    Sensor.addField("temperature", temperature);
+    Serial.print("\t");
+    Serial.print("Humidity:");
+    Serial.println(humidity);
+    Sensor.addField("humidity", humidity);
+  }
+  // Write point
+  if (!client.writePoint(Sensor)) {
+    Serial.print("InfluxDB write failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
+}
